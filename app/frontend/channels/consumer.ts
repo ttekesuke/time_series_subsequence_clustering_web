1a98f485af263164188da8b86520aeb9b255d796
ttekesuke <tekesuke1986@hotmail.com>
2025-04-20 01:12:40 +0900

    set cable-url directly

--- app/frontend/channels/consumer.ts
+++ app/frontend/channels/consumer.ts
@@ -1,6 +1,6 @@
 
 import { createConsumer } from "@rails/actioncable"
-const cableUrl = (import.meta as any).env.VITE_CABLE_URL || "ws://localhost:3000/cable"
+const cableUrl = (import.meta as any).env.VITE_CABLE_URL || "wss://time-series-subsequence-clustering-web.onrender.com/cable"
 const consumer = createConsumer(cableUrl)
 
 export default consumer
