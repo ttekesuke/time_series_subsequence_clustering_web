0696a74571af18972400441feb54b977d4adc713
ttekesuke <tekesuke1986@hotmail.com>
2025-04-20 01:05:59 +0900

    added console.log

--- app/frontend/entrypoints/main/main.ts
+++ app/frontend/entrypoints/main/main.ts
@@ -2,7 +2,7 @@
 import { createApp } from 'vue'
 import vuetify from '../../plugins/vuetify';
 import Main from './Main.vue'
-
+console.log("env.VITE_CABLE_URL:", import.meta.env.VITE_CABLE_URL)
 const app = createApp(Main);
 app.use(vuetify);
 app.mount('#app');
