46a2ab20bc8ce5ebe1f37b2e991a8b48bfdc01f0
ttekesuke <tekesuke1986@hotmail.com>
2026-01-28 23:06:34 +0900

    fixed modules

--- src/TimeseriesClusteringAPI.jl
+++ src/TimeseriesClusteringAPI.jl
@@ -7,9 +7,6 @@ include("timeseries/time_series_cluster_manager.jl")
 
 # ------------------------------------------------------------
 # Polyphonic modules (partial import)
-#
-# generate_polyphonic の完全移植は別途実装しますが、
-# 先行して roughness/STM 周辺から段階的に追加します。
 # ------------------------------------------------------------
 include("polyphonic/polyphonic_config.jl")
 include("polyphonic/dissonance_models.jl")
@@ -19,13 +16,18 @@ include("polyphonic/dissonance_stm_manager.jl")
 include("polyphonic/polyphonic_cluster_manager.jl")
 include("polyphonic/multi_stream_manager.jl")
 
-
 include("controllers/supercolliders_controller.jl")
 include("controllers/time_series_controller.jl")
-const up = Genie.up(async = false)
+
+# up() を“実行せずに”公開する。async は常に false（ブロッキング）に固定
+function up(args...; async::Bool=false, kwargs...)
+  return Genie.up(args...; kwargs..., async=false)
+end
 export up
 
 function main()
+  # Genie.genie() が up() を内部で呼ぶ場合も “同期起動” になるようにする
+  Genie.config.run_as_server = true
   Genie.genie(; context = @__MODULE__)
 end
 
